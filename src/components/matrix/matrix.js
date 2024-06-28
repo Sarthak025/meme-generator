574de273d0d623f43df871ef14e3ec06c99bd3f9
Sarthak025 <sarthaksrivastava025@gmail.com>
2024-06-29 04:59:39 +0530

    added matrix for game

---
+++ src/components/matrix/matrix.js
@@ -0,0 +1,26 @@
+import './matrix.css'
+import Square from "../square/square"
+export default function Matrix()
+{
+    return (
+        <div>
+        <div>
+            <Square />
+            <Square />
+            <Square />
+        </div>
+        <div>
+            <Square />
+            <Square />
+            <Square />
+        </div>
+        <div>
+            <Square />
+            <Square />
+            <Square />
+        </div>
+    </div>
+    )
+
+    
+}
